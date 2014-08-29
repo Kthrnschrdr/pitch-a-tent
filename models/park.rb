b5b7c6f46b9812872c7a2cb2b57995d3d8114112
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-08-29 15:26:30 -0500

    "Sweet-us-ness"

--- models/park.rb
+++ models/park.rb
@@ -3,9 +3,10 @@
 class Park < ActiveRecord::Base
   has_and_belongs_to_many :people
   has_many :stories
-  extend Geocoder::Model::ActiveRecord
  
-  reverse_geocoded_by :latitude, :longitude
+  extend Geocoder::Model::ActiveRecord
+  geocoded_by :address   # can also be an IP address
+  after_validation :geocode 
 
   def self.import_parks
   
@@ -18,72 +19,10 @@ class Park < ActiveRecord::Base
       city = p[1].css('span.bodysmall').text
       
       last_name.gsub!(/\r\n/, '')
-      binding.pry
     
       Park.create(name: "#{first_name} #{last_name}", address: "#{street_address} #{city}")
-      binding.pry
-    end    
-    
-    
-    
-    
-    
-    
-    # x = page.css('span.bodysmall_bold')
-    # parks = []
-    # x.each do |park|
-    #   parks << park.text
-    # end
-    #
-    # parks.delete_if {|text| text == "More information"}
-    # z = []
-    # while parks.length >0
-    #   z << parks.pop(2)
-    # end
-    #
-    # park_names = []
-    # z.each do |p|
-    #   park_names << p.join(' ')
-    # end
-    #
-    # final_park_names = []
-    # park_names.each do |p|
-    #   final_park_names << p.gsub(/\r\n/, '')
-    # end
-    
-    # page = Nokogiri::HTML(open("http://outdoornebraska.ne.gov/parks/guides/contact.asp"))
-    # x = page.css('span.bodysmall')
-  #   parks = []
-  #   x.each do |park|
-  #     parks << park.text
-  #   end
-  #
-  #   parks.keep_if {|text| text =~ (/[NE]\d\d\d\d\d.\d\d\d\d/)}
-  #   z = []
-  #   while parks.length >0
-  #     z << parks.pop(1)
-  #   end
-  #
-  #   final_park_addresses = []
-  #   z.each do |p|
-  #     final_park_addresses << p.join(",")
-  #   end
-    
-    for n in 0...final_park_names.length do
-      Park.create(name: final_park_names[n], address: final_park_addresses[n])
-      binding.pry
+      
     end
-    binding.pry
-  end 
-
+  end    
+   
 end
-
-
-
-# park_map = []
-# page = Nokogiri::HTML(open("))
-# x = page.css('ul.bodysmall a').map { |link| link['href'] }
-# x.each do |p|
-#   y = "http://outdoornebraska.ne.gov" + p
-#   park_map << y
-# end
