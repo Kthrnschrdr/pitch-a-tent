f2ac31c3652a24a062842a3e6a8dfab06da80aab
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-05 14:29:18 -0500

    restructuring files for jquery

--- models/park.rb
+++ models/park.rb
@@ -1,5 +1,5 @@
 
-#this class has the following attributes: name, latitude, longitude, address, webaddress
+#this class has the following attributes: name, latitude, longitude, address, park_url
 class Park < ActiveRecord::Base
   has_and_belongs_to_many :people
   has_many :stories
@@ -21,7 +21,7 @@ class Park < ActiveRecord::Base
       
       last_name.gsub!(/\r\n/, '')
     
-      Park.create(name: "#{first_name} #{last_name}", address: "#{street_address} #{city}", webaddress: "http://outdoornebraska.ne.gov/ + #{webaddress}")
+      Park.create(name: "#{first_name} #{last_name}", address: "#{street_address} #{city}", park_url: "http://outdoornebraska.ne.gov/ + #{webaddress}")
       
     end
   end    
